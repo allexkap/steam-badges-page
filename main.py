384d8fe08e008bf307c40396f0af11e38c2d7340
allexkap <aekpstn@gmail.com>
2025-05-21 13:24:22 +0300

    add min poc

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from steam-badges-page!")
+import json
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+data = json.load(open('./db.json'))
 
 
-if __name__ == "__main__":
-    main()
+@app.route('/')
+def index():
+    # Pass all apps and their badges to the template, and format preview_url for each app
+    preview_url_template = data.get('preview_url')
+    for app in data['apps']:
+        for badge in app['badges']:
+            badge['img_url'] = data['badge_url'].format(appid=app['appid'], hash=badge['hash'])
+        # Always set preview_url for each app using the template
+        app['preview_url'] = preview_url_template.format(appid=app['appid'])
+    return render_template('index.html', apps=data['apps'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
